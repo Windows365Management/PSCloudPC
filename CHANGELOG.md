75361ca94fedb3af93c1f384458ba192dfcc5e66
info@stefandingemanse.com <info@stefandingemanse.com>
2023-01-16 19:12:55 +0100

    1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
-## 1..0.0
+## 1.0.3 [16-1-2023]
+### Bugfixes
+- Bug fixed in New-CPCUserSettingsPolicy, wrong parameter variable.
+### Changes
+
+
+### 
+
+## 1.0.0 [11-1-2023]
 
 - Initial release
--- Compiler/Module.tests.ps1
+++ Compiler/Module.tests.ps1
@@ -32,21 +32,21 @@ Describe "$module Global module tests" {
         It "$module should have a semver version" {
             $moduleContent.ModuleVersion -match "^([0-9]).([0-9]).([0-9])$" | Should -Be $true
         }
-#        It "$module version should be greater than PSGallery" {
-#            $galleryVersion = (Find-Module -Name $module -Repository PSGallery).Version
-#            [version]$moduleContent.ModuleVersion | Should -BeGreaterThan ([version]$galleryVersion)
-#        }     
+        It "$module version should be greater than PSGallery" {
+            $galleryVersion = (Find-Module -Name $module -Repository PSGallery).Version
+            [version]$moduleContent.ModuleVersion | Should -BeGreaterThan ([version]$galleryVersion)
+        }     
         
-#        It "$module project URL should reachable" {
-#            try {
-#                Invoke-WebRequest -Uri $moduleContent.PrivateData.PSData.ProjectUri -UseBasicParsing -DisableKeepAlive -method Head | Out-Null
-#                $exists = $true
-#            }
-#            catch {
-#                $exists = $false
-#            }
-#            $exists | Should -Be $true
-#        }
+        It "$module project URL should reachable" {
+            try {
+                Invoke-WebRequest -Uri $moduleContent.PrivateData.PSData.ProjectUri -UseBasicParsing -DisableKeepAlive -method Head | Out-Null
+                $exists = $true
+            }
+            catch {
+                $exists = $false
+            }
+            $exists | Should -Be $true
+        }
 
         It "$module is valid PowerShell code" {
             $psFile = Get-Content -Path (Join-Path -Path $modulePath -ChildPath $binaryFile) -ErrorAction Stop
