9fe96e46cb5980eb8bc53179102cd0e6e002c03c
info@stefandingemanse.com <info@stefandingemanse.com>
2023-01-19 10:01:55 +0100

    Initial Release OrganizationSettings

---
+++ PSCloudPC/Public/Get-CPCOrganizationSettings.ps1
@@ -0,0 +1,44 @@
+function Get-CPCOrganizationSettings {
+    <#
+    .SYNOPSIS
+    Returns the Cloud PC Organization Settings 
+    .DESCRIPTION
+    This function will return all Cloud PC Organization Settings
+    .EXAMPLE
+    Get-CPCOrganizationSettings
+
+#>
+    Begin {
+        Get-TokenValidity
+        Write-Verbose "Setting url to https://graph.microsoft.com/$script:MSGraphVersion/deviceManagement/virtualEndpoint/organizationSettings"
+        $url = "https://graph.microsoft.com/$script:MSGraphVersion/deviceManagement/virtualEndpoint/organizationSettings"
+                
+    } 
+    
+    Process {
+        
+        #$result = Invoke-WebRequest -uri $url -Method GET -Headers $script:authHeader
+        $result = Invoke-WebRequest -uri $url -Method GET -Headers $script:authHeader
+        
+        if ($null -eq $result) {
+            Write-Error "No Organization Settings returned"
+            break
+        }
+    
+        $resultnew = $result.content | ConvertFrom-Json
+        $resultnew | ForEach-Object {
+        
+            $SettingsList = [PSCustomObject]@{
+                osVersion           = $_.osVersion
+                userAccountType     = $_.userAccountType
+                enableMEMAutoEnroll = $_.enableMEMAutoEnroll
+                enableSingleSignOn  = $_.enableSingleSignOn
+                windowsSettings     = $_.windowsSettings
+            }
+            $SettingsList
+        
+        }
+        
+    }
+    
+}
