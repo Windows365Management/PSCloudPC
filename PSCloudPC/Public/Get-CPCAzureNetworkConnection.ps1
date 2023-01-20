eb081b744365c712c08607bc53f6ebeeb4ddd471
info@stefandingemanse.com <info@stefandingemanse.com>
2023-01-20 12:14:14 +0100

    AB#68 Final version

--- PSCloudPC/Public/Get-CPCAzureNetworkConnection.ps1
+++ PSCloudPC/Public/Get-CPCAzureNetworkConnection.ps1
@@ -1,16 +1,16 @@
 function Get-CPCAzureNetworkConnection {
     <#
     .SYNOPSIS
-    Returns all User Setting Policy's or User Setting Policy's with a specific name
+    Returns all Azure Network Connections or a specific Azure Network Connection
     .DESCRIPTION
-    The function will return all User Setting Policy's or User Setting Policy's with a specific name
+    Returns all Azure Network Connections or a specific Azure Network Connection
     .PARAMETER name
-    Enter the name of the User Setting Policy
+    Name of the Azure Network Connection to return
     .EXAMPLE
-    Get-CPCUserSettingsPolicy -name "UserSettingPolicy01"
-
+    Get-CPCAzureNetworkConnection
+    .EXAMPLE
+    Get-CPCAzureNetworkConnection -Name "Your Azure Network Connection"
 #>
-
     [CmdletBinding()]
     param (
         [parameter(ParameterSetName = "DisplayName")]
@@ -19,7 +19,6 @@ function Get-CPCAzureNetworkConnection {
     
     Begin {
         Get-TokenValidity
-
         switch ($PsCmdlet.ParameterSetName) {
             DisplayName {
                 Write-Verbose "DisplayName parameter provided"
@@ -33,31 +32,29 @@ function Get-CPCAzureNetworkConnection {
     
     Process {
         write-verbose $url
-        $result = Invoke-WebRequest -uri $url -Method GET -Headers $script:authHeader
+        $result = Invoke-WebRequest -uri $url -Method GET -Headers $script:authHeader -SkiphttpErrorCheck
     
         if ($null -eq $result) {
             Write-Error "No Azure Network Connection found"
             break
         }
-
         $resultnew = $result.content | ConvertFrom-Json
         $resultnew.value | ForEach-Object {
     
             $Info = [PSCustomObject]@{
-                id                   = $_.id
-                displayName          = $_.displayName
-                subscriptionId   = $_.subscriptionId
-                type    = $_.type
-                subscriptionName      = $_.subscriptionName
-                adDomainName = $_.adDomainName
-                adDomainUsername  = $_.adDomainUsername
-                organizationalUnit = $_.organizationalUnit
-                resourceGroupId = $_.resourceGroupId
+                id                     = $_.id
+                displayName            = $_.displayName
+                subscriptionId         = $_.subscriptionId
+                type                   = $_.type
+                subscriptionName       = $_.subscriptionName
+                adDomainName           = $_.adDomainName
+                adDomainUsername       = $_.adDomainUsername
+                organizationalUnit     = $_.organizationalUnit
+                resourceGroupId        = $_.resourceGroupId
                 virtualNetworkLocation = $_.virtualNetworkLocation
-                virtualNetworkId = $_.virtualNetworkId
-                subnetId = $_.subnetId
-                healthCheckStatus = $_.healthCheckStatus
-
+                virtualNetworkId       = $_.virtualNetworkId
+                subnetId               = $_.subnetId
+                healthCheckStatus      = $_.healthCheckStatus
             }
             $Info
     
