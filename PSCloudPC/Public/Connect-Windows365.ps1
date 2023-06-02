a5fb4cad872e7f61cb86427ac0147f9fbccdea4f
NKO-MAC <niels@nko-it.nl>
2023-06-02 16:05:37 +0200

    ConverttoDeviceCode

--- PSCloudPC/Public/Connect-Windows365.ps1
+++ PSCloudPC/Public/Connect-Windows365.ps1
@@ -6,6 +6,8 @@ function Connect-Windows365 {
     Connect to Windows 365 via Powershell via Interactive Browser or Service Principal
     .PARAMETER Authtype
     Type of Authentication to use.  Interactive or ServicePrincipal
+    .PARAMETER PowershellVersion
+    Type of Powershell to use.  Windows or Core
     .PARAMETER ClientSecret
     Client Secret for Service Principal Authentication
     .PARAMETER TenantID
@@ -20,11 +22,8 @@ function Connect-Windows365 {
     [CmdletBinding(DefaultParameterSetName = 'Interactive')]
     param (
         
-        [ValidateSet('ServicePrincipal', 'Interactive')]
+        [ValidateSet('ServicePrincipal', 'Interactive', 'DeviceCode')]
         [string]$Authtype = 'Interactive',
-
-        [ValidateSet('Core', 'Windows')]
-        [string]$PowershellVersion = 'Windows',
     
         [Parameter(mandatory = $false)][string]$ClientSecret,
 
@@ -42,105 +41,118 @@ function Connect-Windows365 {
         switch ($Authtype) {
             Interactive {
                 Write-Verbose "Use Interactive Authentication"
-                switch ($PowershellVersion) {
-                    Windows {
-                        Write-Verbose "Using Windows Powershell"
-                        # Add required assemblies
-                        $ClientID = "14d82eec-204b-4c2f-b7e8-296a70dab67e"
-                        $Scopes = "CloudPC.ReadWrite.All%20DeviceManagementConfiguration.ReadWrite.All%20DeviceManagementManagedDevices.ReadWrite.All%20Directory.Read.All"
-                        $redirectUri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+                Write-Verbose "Using Windows Powershell"
+                # Add required assemblies
+                $ClientID = "14d82eec-204b-4c2f-b7e8-296a70dab67e"
+                $Scopes = "CloudPC.ReadWrite.All%20DeviceManagementConfiguration.ReadWrite.All%20DeviceManagementManagedDevices.ReadWrite.All%20Directory.Read.All"
+                $redirectUri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
 
-                        # With User Interaction for Delegated Permission
-                        Add-Type -AssemblyName System.Web
+                # With User Interaction for Delegated Permission
+                Add-Type -AssemblyName System.Web
 
-                        Function Get-AuthCode {
-                            Add-Type -AssemblyName System.Windows.Forms
+                Function Get-AuthCode {
+                    Add-Type -AssemblyName System.Windows.Forms
 
-                            $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width = 640; Height = 840 }
-                            $web = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width = 620; Height = 800; Url = ($url -f ($Scope -join "%20")) }
+                    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width = 640; Height = 840 }
+                    $web = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width = 620; Height = 800; Url = ($url -f ($Scope -join "%20")) }
 
-                            $DocComp = {
-                                $Script:uri = $web.Url.AbsoluteUri        
-                                if ($Script:uri -match "error=[^&]*|code=[^&]*") { $form.Close() }
-                            }
-                            $web.ScriptErrorsSuppressed = $true
-                            $web.Add_DocumentCompleted($DocComp)
-                            $form.Controls.Add($web)
-                            $form.Add_Shown( { $form.Activate() })
-                            $form.ShowDialog() | Out-Null
-
-                            $queryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
-                            $output = @{}
-                            foreach ($key in $queryOutput.Keys) {
-                                $output["$key"] = $queryOutput[$key]
-                            }
-                        }
-                        $url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=$($ClientID)&response_type=code&redirect_uri=$($redirectUri)&response_mode=query&scope=$($Scopes)&state=12345"
-                        Get-AuthCode
-                        # Extract Access token from the returned URI
-                        $regex = '(?<=code=)(.*)(?=&)'
-                        $authCode = ($uri | Select-string -pattern $regex).Matches[0].Value
-
-                        Write-Verbose "Received an authCode, $authCode"
-
-                        # get Access Token
-                        $body = "grant_type=authorization_code&redirect_uri=$redirectUri&client_id=$clientId&code=$authCode"
-                        $connection = Invoke-RestMethod https://login.microsoftonline.com/common/oauth2/token `
-                            -Method Post -ContentType "application/x-www-form-urlencoded" `
-                            -Body $body `
-                            -ErrorAction STOP
-                        # Access Token
-                        $Token = $connection.access_token
-                        $script:Authtime = [System.DateTime]::UtcNow
-                        $script:Authtoken = $connection
-                        $script:Authheader = @{Authorization = "Bearer $($Token)" }
+                    $DocComp = {
+                        $Script:uri = $web.Url.AbsoluteUri        
+                        if ($Script:uri -match "error=[^&]*|code=[^&]*") { $form.Close() }
                     }
-                    Core {
-                        Write-Verbose "Using Powershell Core"
+                    $web.ScriptErrorsSuppressed = $true
+                    $web.Add_DocumentCompleted($DocComp)
+                    $form.Controls.Add($web)
+                    $form.Add_Shown( { $form.Activate() })
+                    $form.ShowDialog() | Out-Null
 
-                        $ClientID = "14d82eec-204b-4c2f-b7e8-296a70dab67e"
-                        $Scopes = "CloudPC.ReadWrite.All%20DeviceManagementConfiguration.ReadWrite.All%20DeviceManagementManagedDevices.ReadWrite.All%20Directory.Read.All"
-                        $redirectUri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
-
-                        $url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=$($ClientID)&response_type=code&redirect_uri=$($redirectUri)&response_mode=query&scope=$($Scopes)&state=12345"
-
-                        # get Access Token
-                        $body = "grant_type=authorization_code&redirect_uri=$redirectUri&client_id=$clientId&code=$authCode"
-                        $connection = Invoke-RestMethod https://login.microsoftonline.com/common/oauth2/token `
-                            -Method Post -ContentType "application/x-www-form-urlencoded" `
-                            -Body $body `
-                            -ErrorAction STOP
-                        
-                        $Token = $connection.access_token  
-
-                        $script:Authtime = [System.DateTime]::UtcNow
-                        $script:Authtoken = $connection
-                        $script:Authheader = @{Authorization = "Bearer $($Token)" }
+                    $queryOutput = [System.Web.HttpUtility]::ParseQueryString($web.Url.Query)
+                    $output = @{}
+                    foreach ($key in $queryOutput.Keys) {
+                        $output["$key"] = $queryOutput[$key]
                     }
                 }
-                
-            }
+                $url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=$($ClientID)&response_type=code&redirect_uri=$($redirectUri)&response_mode=query&scope=$($Scopes)&state=12345"
+                Get-AuthCode
+                # Extract Access token from the returned URI
+                $regex = '(?<=code=)(.*)(?=&)'
+                $authCode = ($uri | Select-string -pattern $regex).Matches[0].Value
 
-            ServicePrincipal {
+                Write-Verbose "Received an authCode, $authCode"
 
-                $body = @{
-                    Grant_Type    = "client_credentials"
-                    Scope         = "https://graph.microsoft.com/.default"
-                    Client_Id     = $ClientID
-                    Client_Secret = $ClientSecret
-                }
-                
-                $connection = Invoke-RestMethod `
-                    -Uri https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token `
-                    -Method POST `
-                    -Body $body
-                
-                $token = $connection.access_token
-        
+                # get Access Token
+                $body = "grant_type=authorization_code&redirect_uri=$redirectUri&client_id=$clientId&code=$authCode"
+                $connection = Invoke-RestMethod https://login.microsoftonline.com/common/oauth2/token `
+                    -Method Post -ContentType "application/x-www-form-urlencoded" `
+                    -Body $body `
+                    -ErrorAction STOP
+                # Access Token
+                $Token = $connection.access_token
                 $script:Authtime = [System.DateTime]::UtcNow
                 $script:Authtoken = $connection
-                $script:Authheader = @{Authorization = "Bearer $($Token)" }
+                $script:Authheader = @{Authorization = "Bearer $($Token)" }                   
             }
+                
+        }
+        DeviceCode {
+            Write-Verbose "Using Device Code"
+            $TenantID = "7l4l1k.onmicrosoft.com"
+            $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"
+            $scope = "CloudPC.ReadWrite.All%20DeviceManagementConfiguration.ReadWrite.All%20DeviceManagementManagedDevices.ReadWrite.All%20Directory.Read.All"
+
+            $clientBody = @{
+                client_id = $ClientId
+                tenant    = $TenantID
+                scope     = $Scope
+            }
+            $requestUrl = Invoke-WebRequest -Method POST -Uri "https://login.microsoftonline.com/$($TenantID)/oauth2/v2.0/devicecode" -Body $clientBody
+            $content = ($requestUrl.Content | ConvertFrom-Json)
+            if ([string]::IsNullOrEmpty($script:Authtoken.access_token)) {
+                Write-Information "`n$($content.message)" -InformationAction Continue
+            }
+            # Get OAuth Token
+            $tokenBody = @{
+                grant_type = "urn:ietf:params:oauth:grant-type:device_code"
+                code       = $content.device_code
+                client_id  = $ClientId
+            }
+            while ([string]::IsNullOrEmpty($script:Authtoken.access_token)) {
+                $script:Authtoken = try {
+                    Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$TenantID/oauth2/token" -Body $tokenBody
+                }
+                catch {
+                    $errorMessage = $_.ErrorDetails.Message | ConvertFrom-Json
+                    # If not waiting for auth, throw error
+                    if ($errorMessage.error -ne "authorization_pending") {
+                        throw "Authorization is pending."
+                    }
+                }
+            }
+
+            $script:Authtime = [System.DateTime]::UtcNow
+            $script:Authtoken = $connection
+            $script:Authheader = @{Authorization = "Bearer $($Token)" }
+        }
+
+        ServicePrincipal {
+
+            $body = @{
+                Grant_Type    = "client_credentials"
+                Scope         = "https://graph.microsoft.com/.default"
+                Client_Id     = $ClientID
+                Client_Secret = $ClientSecret
+            }
+                
+            $connection = Invoke-RestMethod `
+                -Uri https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token `
+                -Method POST `
+                -Body $body
+                
+            $token = $connection.access_token
+        
+            $script:Authtime = [System.DateTime]::UtcNow
+            $script:Authtoken = $connection
+            $script:Authheader = @{Authorization = "Bearer $($Token)" }
         }
     }
 }
