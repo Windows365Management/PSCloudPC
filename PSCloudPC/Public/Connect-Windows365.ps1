3848ca5735e6ab53b8583743b35d9aa1f82db2ff
NielsKok <Ruthhl3ss>
2024-09-25 10:48:15 +0200

    AddedCertificateBasedAuth

--- PSCloudPC/Public/Connect-Windows365.ps1
+++ PSCloudPC/Public/Connect-Windows365.ps1
@@ -12,12 +12,16 @@ function Connect-Windows365 {
     Tenant ID for all Authentication types
     .PARAMETER ClientID
     Client ID for Service Principal Authentication
+    .PARAMETER ClientCertificate
+    Client Certificate for Service Principal Authentication (THUMBPRINT)
     .EXAMPLE
     Connect-Windows365 -TenantID contoso.onmicrosoft.com
     .EXAMPLE
     Connect-Windows365 -AuthType DeviceCode -TenantID contoso.onmicrosoft.com
     .EXAMPLE
-    Connect-Windows365 -Authtype ServicePrincipal -TenantID contoso.onmicrosoft.com -ClientID 12345678-1234-1234-1234-123456789012 -ClientSecret 12345678-1234-1234-1234-123456789012   
+    Connect-Windows365 -Authtype ClientSecret -TenantID contoso.onmicrosoft.com -ClientID 12345678-1234-1234-1234-123456789012 -ClientSecret 12345678-1234-1234-1234-123456789012   
+    .EXAMPLE
+    Connect-Windows365 -Authtype ClientCertificate -TenantID contoso.onmicrosoft.com -ClientID 12345678-1234-1234-1234-123456789012 -ClientCertificate "THUMBPRINT"
     #>
     [CmdletBinding(DefaultParameterSetName = 'Interactive')]
     param (
@@ -35,7 +39,10 @@ function Connect-Windows365 {
         [string]$TenantID,
 
         [parameter(Mandatory, ParameterSetName = "ServicePrincipal")]
-        [string]$ClientID
+        [string]$ClientID,
+        
+        [parameter(Mandatory, ParameterSetName = "ServicePrincipal")]
+        [string]$ClientCertificate
     )
     begin {
         # Set the profile to beta
@@ -50,7 +57,7 @@ function Connect-Windows365 {
                 $environment = Get-ChildItem -Path C:\Windows -ErrorAction SilentlyContinue
 
                 If ($null -eq $environment) {
-                    Write-Error "Using Powershell Core on Mac or Linux, please use the DeviceCode or ServicePrincipal Authentication"
+                    Write-Error "Using Powershell Core on Mac or Linux, please use the DeviceCode or ClientSecret Authentication"
                     Break
                 }
                 else {
@@ -154,7 +161,9 @@ function Connect-Windows365 {
                 $script:Authheader = @{Authorization = "Bearer $($Token)" }
             }
 
-            ServicePrincipal {
+            ClientSecret {
+
+                Write-Verbose "Using Client Secret Authentication"
 
                 $body = @{
                     Grant_Type    = "client_credentials"
@@ -173,6 +182,118 @@ function Connect-Windows365 {
                 $script:Authtime = [System.DateTime]::UtcNow
                 $script:Authtoken = $connection
                 $script:Authheader = @{Authorization = "Bearer $($Token)" }
+            }
+
+            ClientCertificate {
+                $environment = Get-Item Cert:\LocalMachine\My -ErrorAction SilentlyContinue
+
+                If ($null -eq $environment) {
+                    Write-Error "Using Powershell Core on Mac or Linux, please use the DeviceCode or ClientSecret Authentication"
+                    Break
+                }
+                else {
+                    Write-Verbose "Using Windows Powershell (Core), continue with the script"
+                    $Certificate = Get-Item Cert:\LocalMachine\My\$ClientCertificate #thumbprint of the cert
+                }
+
+                Write-Verbose "Using Client Certificate Authentication"
+                $TenantName = $TenantID
+                $AppId = $ClientID
+                $Scope = "https://graph.microsoft.com/.default"
+
+                # Create base64 hash of certificate
+                $CertificateBase64Hash = [System.Convert]::ToBase64String($Certificate.GetCertHash())
+
+                # Create JWT timestamp for expiration
+                $StartDate = (Get-Date "1970-01-01T00:00:00Z" ).ToUniversalTime()
+                $JWTExpirationTimeSpan = (New-TimeSpan -Start $StartDate -End (Get-Date).ToUniversalTime().AddMinutes(2)).TotalSeconds
+                $JWTExpiration = [math]::Round($JWTExpirationTimeSpan, 0)
+
+                # Create JWT validity start timestamp
+                $NotBeforeExpirationTimeSpan = (New-TimeSpan -Start $StartDate -End ((Get-Date).ToUniversalTime())).TotalSeconds
+                $NotBefore = [math]::Round($NotBeforeExpirationTimeSpan, 0)
+
+                # Create JWT header
+                $JWTHeader = @{
+                    alg = "RS256"
+                    typ = "JWT"
+                    # Use the CertificateBase64Hash and replace/strip to match web encoding of base64
+                    x5t = $CertificateBase64Hash -replace '\+', '-' -replace '/', '_' -replace '='
+                }
+
+                # Create JWT payload
+                $JWTPayLoad = @{
+                    # What endpoint is allowed to use this JWT
+                    aud = "https://login.microsoftonline.com/$TenantName/oauth2/token"
+                    # Expiration timestamp
+                    exp = $JWTExpiration
+                    # Issuer = your application
+                    iss = $AppId
+                    # JWT ID: random guid
+                    jti = [guid]::NewGuid()
+                    # Not to be used before
+                    nbf = $NotBefore
+                    # JWT Subject
+                    sub = $AppId
+                }
+
+                # Convert header and payload to base64
+                $JWTHeaderToByte = [System.Text.Encoding]::UTF8.GetBytes(($JWTHeader | ConvertTo-Json))
+                $EncodedHeader = [System.Convert]::ToBase64String($JWTHeaderToByte)
+
+                $JWTPayLoadToByte = [System.Text.Encoding]::UTF8.GetBytes(($JWTPayload | ConvertTo-Json))
+                $EncodedPayload = [System.Convert]::ToBase64String($JWTPayLoadToByte)
+
+                # Join header and Payload with "." to create a valid (unsigned) JWT
+                $JWT = $EncodedHeader + "." + $EncodedPayload
+
+                # Get the private key object of your certificate
+                # $PrivateKey = $Certificate.PrivateKey
+                $PrivateKey = ([System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($Certificate))
+
+                # Define RSA signature and hashing algorithm
+                $RSAPadding = [Security.Cryptography.RSASignaturePadding]::Pkcs1
+                $HashAlgorithm = [Security.Cryptography.HashAlgorithmName]::SHA256
+
+                # Create a signature of the JWT
+                $Signature = [Convert]::ToBase64String(
+                    $PrivateKey.SignData([System.Text.Encoding]::UTF8.GetBytes($JWT), $HashAlgorithm, $RSAPadding)
+                ) -replace '\+', '-' -replace '/', '_' -replace '='
+
+                # Join the signature to the JWT with "."
+                $JWT = $JWT + "." + $Signature
+
+                # Create a hash with body parameters
+                $Body = @{
+                    client_id             = $AppId
+                    client_assertion      = $JWT
+                    client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+                    scope                 = $Scope
+                    grant_type            = "client_credentials"
+                }
+
+                $Url = "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token"
+
+                # Use the self-generated JWT as Authorization
+                $Header = @{
+                    Authorization = "Bearer $JWT"
+                }
+
+                # Splat the parameters for Invoke-Restmethod for cleaner code
+                $PostSplat = @{
+                    ContentType = 'application/x-www-form-urlencoded'
+                    Method      = 'POST'
+                    Body        = $Body
+                    Uri         = $Url
+                    Headers     = $Header
+                }
+
+                $Request = Invoke-RestMethod @PostSplat
+
+                $script:Authtime = [System.DateTime]::UtcNow
+                $script:Authtoken = $connection
+                $script:Authheader = @{Authorization = "Bearer $($Request.access_token)" }
+
             }
         }
     }
