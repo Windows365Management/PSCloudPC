0bfd0b1aac171fe4b2638a07cb266f55e27573e3
Niels Kok <>
2026-03-18 13:43:11 +0100

    UpdatedPath

--- Pester/Files.tests.ps1
+++ Pester/Files.tests.ps1
@@ -1,4 +1,4 @@
-$modulePath = Join-Path -Path (Join-Path ".././" -ChildPath "PSCloudPC") -ChildPath "Src"
+$modulePath = Join-Path -Path (Join-Path ".././" -ChildPath "PSCloudPC") -ChildPath "PSCloudPc"
 $psFiles = Get-ChildItem -Path (Join-Path -Path $modulePath -ChildPath "Public")
 
 Describe "Analyze code" -ForEach @(
