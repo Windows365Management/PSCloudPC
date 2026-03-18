69dd3c092575af75b0771b4a6760a6b87a0d5363
Niels Kok <47400512+Ruthhl3ss@users.noreply.github.com>
2026-03-18 12:32:59 +0100

    Change PS_GALLERY_KEY to securestring type

--- Publish/PublishPSGallery.ps1
+++ Publish/PublishPSGallery.ps1
@@ -1,14 +1,14 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string]$PS_GALLERY_KEY
+    [securestring]$PS_GALLERY_KEY
 )
 
 # Install required module
 Install-Module -Name MSAL.PS -Force -Scope CurrentUser
 
 # Set project name
-$env:ProjectName = "Src"
+$env:ProjectName = "PSCloudPc"
 
 # Build the correct module path relative to the Publish directory
 # When running from the repo root, the module manifest is at ./Src/PSCloudPC.psd1
