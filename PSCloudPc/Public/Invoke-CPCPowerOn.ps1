262041d3031b1ac9f90d7dcb612b4ddf89e69283
Yoda (OpenClaw Agent) <nielsopenclaw@agentmail.to>
2026-04-13 04:08:02 +0200

    Fix Invoke-CPCPowerOn/Off: handle array from Get-CloudPC, add -CloudPCId param (closes #107)

--- PSCloudPc/Public/Invoke-CPCPowerOn.ps1
+++ PSCloudPc/Public/Invoke-CPCPowerOn.ps1
@@ -7,49 +7,72 @@ function Invoke-CPCPowerOn {
     Microsoft Graph beta API. After the Cloud PC is powered on, it is allocated
     to a user and licenses are assigned immediately.
 
+    You can identify the target Cloud PC by its managed device name (default) or
+    by providing the Cloud PC object ID directly via -CloudPCId.
+
     Note: This action applies to Windows 365 Frontline Cloud PCs only.
     Only IT admin users can perform this action. Returns 204 No Content on success.
     .PARAMETER Name
-    Enter the display name of the Cloud PC to power on
+    The managed device name of the Cloud PC to power on. Use Get-CloudPC to find
+    Cloud PC names. Mutually exclusive with -CloudPCId.
+    .PARAMETER CloudPCId
+    The object ID (GUID) of the Cloud PC to power on. Use Get-CloudPC to find
+    Cloud PC IDs. Mutually exclusive with -Name.
     .EXAMPLE
-    Invoke-CPCPowerOn -Name "CloudPC01"
+    Invoke-CPCPowerOn -Name "CPC-User-XXXX"
     .EXAMPLE
-    Invoke-CPCPowerOn -Name "CloudPC01" -WhatIf
+    Invoke-CPCPowerOn -CloudPCId "4b5ad5e0-6a0b-4ffc-818d-36bb23cf4dbd"
+    .EXAMPLE
+    Invoke-CPCPowerOn -Name "CPC-User-XXXX" -WhatIf
     .NOTES
     Requires CloudPC.ReadWrite.All permission (delegated or application).
     This action uses the Microsoft Graph beta endpoint.
     API reference: https://learn.microsoft.com/en-us/graph/api/cloudpc-poweron
     #>
-    [CmdletBinding(SupportsShouldProcess = $true)]
+    [CmdletBinding(DefaultParameterSetName = 'Name', SupportsShouldProcess = $true)]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Name')]
         [ValidateNotNullOrEmpty()]
-        [string]$Name
+        [string]$Name,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Id')]
+        [ValidateNotNullOrEmpty()]
+        [string]$CloudPCId
     )
 
     begin {
         Get-TokenValidity
 
-        $CloudPC = Get-CloudPC -Name $Name
+        if ($PSCmdlet.ParameterSetName -eq 'Name') {
+            $CloudPC = Get-CloudPC -Name $Name
 
-        if ($null -eq $CloudPC) {
-            Throw "No Cloud PC found with name '$Name'"
-            return
+            if ($null -eq $CloudPC -or @($CloudPC).Count -eq 0) {
+                Throw "No Cloud PC found with name '$Name'. Use Get-CloudPC to verify the managed device name."
+            }
+
+            # Take the first result in case the filter returns multiple
+            $CloudPC = @($CloudPC)[0]
+            $targetId   = $CloudPC.id
+            $targetName = $CloudPC.displayName
+        }
+        else {
+            $targetId   = $CloudPCId
+            $targetName = $CloudPCId
         }
 
-        # powerOn is a beta-only API; always use beta endpoint
-        $url = "https://graph.microsoft.com/beta/deviceManagement/virtualEndpoint/cloudPCs/$($CloudPC.id)/powerOn"
+        # powerOn is a beta-only API; always use /beta/ path
+        $url = "https://graph.microsoft.com/beta/deviceManagement/virtualEndpoint/cloudPCs/$targetId/powerOn"
 
         Write-Verbose "URL: $url"
     }
 
     Process {
-        Write-Verbose "Powering on Cloud PC '$($CloudPC.displayName)' (id: $($CloudPC.id))"
+        Write-Verbose "Powering on Cloud PC '$targetName' (id: $targetId)"
 
-        if ($PSCmdlet.ShouldProcess($CloudPC.displayName, "Power on Cloud PC")) {
+        if ($PSCmdlet.ShouldProcess($targetName, "Power on Cloud PC")) {
             try {
                 Invoke-RestMethod -Headers $script:Authheader -Uri $url -Method POST
-                Write-Output "Cloud PC '$($CloudPC.displayName)' power on initiated"
+                Write-Output "Cloud PC '$targetName' power on initiated"
             }
             catch {
                 Throw $_.Exception.Message
